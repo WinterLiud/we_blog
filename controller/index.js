bb40474eec01f7bf8b9b43c062133fc4ac30986a
WinterLiu <ld714805258@163.com>
2018-11-09 20:30:13 +0800

    首页基本渲染

--- controller/index.js
+++ controller/index.js
@@ -4,12 +4,14 @@ module.exports={
         // let pageSize = 3
         // let currentPage = parseInt(req.query.page) || 1
 
-        const sql=`select * from article as a
+        const sql=`select a.id, a.title, a.ctime, u.nickname, u.username from article as a
         left join user as u 
         on a.authorId=u.id
         order by a.id desc;`
         conn.query(sql,(err,result)=>{
            if (!result) result = [] 
+        //    let totalCount=result[1][0].count
+        //    let totalPage=Math.ceil(totalCount/pageSize)
            res.render('./index.ejs',{
             user:req.session.user,
             isLogin:req.session.isLogin, 
